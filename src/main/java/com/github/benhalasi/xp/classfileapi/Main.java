2c884a44add65af032c269924181a00f66bbb841
Benedek Halasi <benhalasi@gmail.com>
2024-06-08 12:08:12 +0200

    Hi there!

---
+++ src/main/java/com/github/benhalasi/xp/classfileapi/Main.java
@@ -0,0 +1,11 @@
+package com.github.benhalasi.xp.classfileapi;
+
+import java.lang.classfile.ClassFile;
+
+@SuppressWarnings("preview")
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println(ClassFile.of());
+  }
+}
